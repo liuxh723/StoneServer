2726c8be66310085cc2eeb95e532bc576dbd5336
liuxh723 <86610076@qq.com>
2018-04-25 15:09:57 +0800

    e

---
+++ kill_server.bat
@@ -0,0 +1,14 @@
+taskkill /f /t /im machine.exe 
+taskkill /f /t /im logger.exe 
+taskkill /f /t /im dbmgr.exe
+taskkill /f /t /im baseappmgr.exe
+
+taskkill /f /t /im cellappmgr.exe
+taskkill /f /t /im baseapp.exe 
+taskkill /f /t /im cellapp.exe 
+
+taskkill /f /t /im loginapp.exe
+
+taskkill /f /t /im interfaces.exe 
+
+taskkill /f /t /im bots.exe 
